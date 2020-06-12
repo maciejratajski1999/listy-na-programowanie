cb76e959cae0240317bdf49370293c202792e2e5
maciejratajski1999 <maciejratajski1999@gmail.com>
2020-06-12 22:31:09 +0200

    added files: resources
    dodany lekki interface, można zliczać punkty

--- Lista-7/game.py
+++ Lista-7/game.py
@@ -7,7 +7,7 @@ pygame.init()
 pygame.mixer.init()
 
 size = width, height = 960, 540
-speed = [0, 5]
+speed = [0, 12]
 background = 120, 22, 10
 blue = 10, 50, 130
 grey = 100, 100, 100
@@ -16,7 +16,8 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Metal Thrashing")
 
 drumstick = pygame.mixer.Sound("resources\\drumstick.wav")
-# metal_E = pygame.mixer.Sound("resources\\metal_E.wav")
+highstick = pygame.mixer.Sound("resources\\highstick.wav")
+kickdrum = pygame.mixer.Sound("resources\\kickdrum.wav")
 
 def game_text(content, font):
     ingame_text = font.render(content, True, black)
@@ -55,10 +56,20 @@ def game_start():
 
 def game_song(key):
 
+    downboard = pygame.image.load("resources\\downboard.png")
+    downboard_rect = downboard.get_rect()
+    downboard_rect.center = (width/2, height-100)
+
     def get_song(key):
         song = Song(key)
-        return Timer(song.bpm), song.tab, song.length, song.background_sounds, song.user_sounds
-    song_timer, overtab, length, bg_sounds, user_sounds = get_song(key)
+        return Timer(song.bpm), song.tab, song.length, song.background_sounds, song.user_sounds, song.bg_image
+
+    song_timer, overtab, length, bg_sounds, user_sounds, bg_image_path = get_song(key)
+
+    bg_image = pygame.image.load(bg_image_path)
+    bg_image = pygame.transform.scale(bg_image, (300,640))
+    bg_image_rect = bg_image.get_rect()
+    bg_image_rect.center = (width/2, height/2)
 
     def new_tab(index):
         try:
@@ -68,44 +79,68 @@ def game_song(key):
             ingame_positions = [pos for image, pos in sprites]
             ingame_notes = [image.get_rect() for image, pos in sprites]
             ingame_sounds = [[pygame.mixer.Sound(note.sound)] for note in notes]
+            ingame_keys = [note.key for note in notes]
             for i in range(0, len(ingame_notes)):
                 ingame_notes[i].x = ingame_positions[i]
                 ingame_notes[i].y = -100
-            return sprites, ingame_notes, ingame_sounds
+            return sprites, ingame_notes, ingame_sounds, ingame_keys
         except IndexError:
-            return [], [], []
+            return [], [], [], []
 
 
-    all_sprites, all_notes, all_sounds = [], [], []
+    all_sprites, all_notes, all_sounds, all_keys = [], [], [], []
+    score = 0
     while 1:
         frame_sounds = []
+        note_check = []
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT : sys.exit()
-            elif keys[pygame.K_q]:frame_sounds.append(pygame.mixer.Sound(user_sounds["Q"]))
-            elif keys[pygame.K_w]:frame_sounds.append(pygame.mixer.Sound(user_sounds["W"]))
-            elif keys[pygame.K_e]:frame_sounds.append(pygame.mixer.Sound(user_sounds["E"]))
-            elif keys[pygame.K_r]:frame_sounds.append(pygame.mixer.Sound(user_sounds["R"]))
+            elif keys[pygame.K_q]:
+                frame_sounds.append(pygame.mixer.Sound(user_sounds["Q"]))
+                note_check.append("Q")
+            elif keys[pygame.K_w]:
+                frame_sounds.append(pygame.mixer.Sound(user_sounds["W"]))
+                note_check.append("W")
+            elif keys[pygame.K_e]:
+                frame_sounds.append(pygame.mixer.Sound(user_sounds["E"]))
+                note_check.append("E")
+            elif keys[pygame.K_r]:
+                frame_sounds.append(pygame.mixer.Sound(user_sounds["R"]))
+                note_check.append("R")
         if song_timer.tab_index > length and len(all_notes) == 0:
             # TODO
-            raise IOError("koniec piosenki niezaimplementowany")
+            print(score)
+            game_start()
 
         if song_timer.check():
             song_timer.tab_index_up()
-            sprites, ingame_notes, ingame_sounds = new_tab(song_timer.tab_index)
+            sprites, ingame_notes, ingame_sounds, ingame_keys = new_tab(song_timer.tab_index)
             for new_sprite in sprites:
                 all_sprites.append(new_sprite)
             for new_note in ingame_notes:
                 all_notes.append(new_note)
             for new_sound in ingame_sounds:
                 all_sounds.append(new_sound)
+            for new_key in ingame_keys:
+                all_keys.append(new_key)
 
             if (song_timer.tab_index % 2) == 0:
                 all_sounds[-1].append(drumstick)
+            if ((song_timer.tab_index + 1) % 8) == 0:
+                all_sounds[-1].append(highstick)
+            if ((song_timer.tab_index - 1) % 4) == 0:
+                all_sounds[-1].append(kickdrum)
 
         __remove = []
         for i in range(len(all_notes)):
             all_notes[i] = all_notes[i].move(speed)
+            if all_keys[i] in note_check:
+                if all_notes[i].top > (height-180):
+                    for sound in all_sounds[i]:
+                        frame_sounds.append(sound)
+                    __remove.append(i)
+                    score += 1
             if all_notes[i].top > (height - 100):
                 __remove.append(i)
                 for sound in all_sounds[i]:
@@ -116,10 +151,14 @@ def game_song(key):
                 all_sprites.pop(index)
                 all_notes.pop(index)
                 all_sounds.pop(index)
+                all_keys.pop(index)
+
 
         screen.fill(background)
+        screen.blit(bg_image, bg_image_rect)
         for j in range(0, len(all_notes)):
-            screen.blit(all_sprites[0 - j][0], all_notes[0 - j])
+            screen.blit(all_sprites[-1 - j][0], all_notes[-1 - j])
+        screen.blit(downboard, downboard_rect)
         pygame.display.flip()
         song_timer()
         pygame.time.Clock().tick_busy_loop(30)
--- Lista-7/song.py
+++ Lista-7/song.py
@@ -2,12 +2,31 @@ class Song:
 
     def __init__(self, key):
         self.key = key
-        self.bpm, self.tab, self.background_sounds, self.user_sounds = self.__translate()
+        self.bpm, self.tab, self.background_sounds, self.user_sounds, self.bg_image = self.__translate()
         self.length = len(self.tab)
 
     def __translate(self):
         self.song_dict = {
-            "metal" : (130, [["Q"], ["Q"], ["W", "R"], ["Q", "E"], [ "E"], ["Q"], ["W"], [ "E"], ["R"], ["Q"], ["W", "R"], ["Q", "E"], ["E"], ["E"], ["E"], ["E"], ["E"], ["E"], ["W"], ["W"], ["W"], ["W"], ["W"], ["W"]], ["resources\\metal_E.wav", "resources\\metal_G.wav", "resources\\metal_A.wav", "resources\\metal_B.wav"], {"Q" : "resources\\user_A.wav", "W" : "resources\\user_C.wav", "E" : "resources\\user_D.wav", "R" : "resources\\user_E.wav"}),
-            "blues" : (97, [["W"], ["E"], [], ["W"], ["W"], ["E"], ["W"], ["E"], ["Q"], ["W"], ["W"], ["E"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["Q"], ["Q"], ["E"], ["W"], ["W"], ["W"], ["E"], ["Q"], ["R"], [], [], ["R"]], ["resources\\metal_E.wav", "resources\\metal_G.wav", "resources\\metal_A.wav", "resources\\metal_B.wav"],{"Q" : "resources\\user_A.wav", "W" : "resources\\user_C.wav", "E" : "resources\\user_D.wav", "R" : "resources\\user_E.wav"})
+            "metal" : (160, [["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["W"],
+                             ["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["W"],
+                             ["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["Q"],["W"],
+                             ["Q"],["E"],["E"],["R"],["Q"],["E"],["E"],["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["E"], ["E"], ["R"], ["Q"], ["E"], ["E"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["E"], ["E"], ["R"], ["Q"], ["E"], ["E"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["Q"], ["W"],
+                             ["Q"], ["E"], ["E"], ["R"], ["Q"], ["E"], ["E"], ["W"]
+                             ], ["resources\\metal_E.wav", "resources\\metal_G.wav", "resources\\metal_A.wav", "resources\\metal_B.wav"], {"Q" : "resources\\user_A.wav", "W" : "resources\\user_C.wav", "E" : "resources\\user_D.wav", "R" : "resources\\user_E.wav"}, "resources\\metalhead.png"),
+            "blues" : (97, [["W"], ["E"], [], ["W"], ["W"], ["E"], ["W"], ["E"], ["Q"], ["W"], ["W"], ["E"],
+                            ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"],
+                            ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"], ["R"], ["R"], ["E"], ["Q"],
+                            ["Q"], ["Q"], ["E"], ["W"], ["W"], ["W"], ["E"], ["Q"], ["R"], [], [], ["R"]], ["resources\\metal_E.wav", "resources\\metal_G.wav", "resources\\metal_A.wav", "resources\\metal_B.wav"],{"Q" : "resources\\user_A.wav", "W" : "resources\\user_C.wav", "E" : "resources\\user_D.wav", "R" : "resources\\user_E.wav"}, "resources\\metalhead.png")
         }
         return self.song_dict[self.key]
